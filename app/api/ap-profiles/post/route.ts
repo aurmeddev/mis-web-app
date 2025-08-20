14e5af30337bd34f0136727162f7efb0b00ec4eb
Jay Lord <pagal.jaylord@gmail.com>
2025-08-21 00:23:29 +0800

    decrypt the session user id

--- app/api/ap-profiles/post/route.ts
+++ app/api/ap-profiles/post/route.ts
@@ -2,6 +2,7 @@ import { query } from "@/database/dbConnection";
 import { PostApProfilesProps } from "@/lib/features/ap-profiles/type/ApProfilesProps";
 import { MySqlUtils } from "@/lib/mysql/MySqlUtils";
 import { ObjectUtils } from "@/lib/object/ObjectUtils";
+import { CryptoServerService } from "@/lib/security/cryptography/CryptoServerService";
 import { getSession } from "@/lib/security/user-auth/jwt/JwtAuthService";
 import { NextResponse, NextRequest } from "next/server";
 export const POST = async (request: NextRequest) => {
@@ -17,12 +18,27 @@ export const POST = async (request: NextRequest) => {
     );
   }
 
+  // Decrypt the user ID from the session
+  const decipher = new CryptoServerService();
+  const { isSuccess, decryptedData } = await decipher.decrypt({
+    data: session.user.id,
+  });
+  if (!isSuccess) {
+    return NextResponse.json(
+      {
+        isSuccess,
+        message: "Data parse error.",
+      },
+      { status: 500 }
+    );
+  }
+
   const data: PostApProfilesProps = await request.json();
   const objUtil = new ObjectUtils();
   const payload = objUtil.removeInvalidKeys(data);
   const mysqlUtils = new MySqlUtils();
   const { columns, values, questionMarksValue } =
-    mysqlUtils.generateInsertQuery({ created_by: 1, ...payload });
+    mysqlUtils.generateInsertQuery({ created_by: decryptedData, ...payload });
   const queryString = `INSERT INTO Ap_Profiles ${columns} ${questionMarksValue}`;
   console.log(queryString);
   console.log(values);

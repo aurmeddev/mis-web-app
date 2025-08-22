14546c120e2f3177dfd75535badfdba8540b11e7
Jay Lord <pagal.jaylord@gmail.com>
2025-08-22 23:39:27 +0800

    refactor v_ApProfiles data structure

--- database/views/v_ApProfiles.sql
+++ database/views/v_ApProfiles.sql
@@ -1,5 +1,5 @@
 SELECT 
-  ap.id AS ap_profile_id,
+  ap.id,
   ap.fb_account_id,
   ap.profile_name,
   ap.marketing_api_access_token,
@@ -15,9 +15,17 @@ SELECT
       'app_2fa_key',fb.app_2fa_key,
       'fb_owner_account_created',fb.fb_owner_account_created,
       'no_of_friends',fb.no_of_friends,
-      'fb_account_quality_status_id',fb.fb_account_quality_status_id,
+      'fb_account_quality',
+       COALESCE((
+        SELECT JSON_OBJECT(
+          'id', qt.id,
+          'status', qt.fb_account_quality
+        )
+        FROM `Fb_Account_Quality_Types` AS qt
+        WHERE qt.id = fb.fb_account_quality_type_id
+      ), JSON_OBJECT()),
       'remarks',fb.remarks,
-      'fb_account_status',COALESCE((
+      'is_active',COALESCE((
         SELECT JSON_OBJECT(
           'id', st.id,
           'status', st.status
@@ -51,7 +59,7 @@ SELECT
     WHERE ap.fb_account_id=fb.id
   ), JSON_OBJECT()) AS fb_account,
   ap.created_at,
-  ap.created_by AS created_by_id,
+  ap.created_by,
   COALESCE((
     SELECT JSON_OBJECT(
       'full_name', u.full_name,
@@ -61,13 +69,13 @@ SELECT
     INNER JOIN `Teams` AS t ON u.team_id = t.id
     WHERE u.id = ap.created_by
   ), JSON_OBJECT()) AS ap_created_by,
-  ap.is_active AS ap_is_active,
+  ap.is_active,
   COALESCE((
     SELECT JSON_OBJECT(
-      'ap_is_active', st.id,
+      'id', st.id,
       'status', st.status
     )
     FROM `Status_Types` AS st
     WHERE st.id = ap.is_active
-  ), JSON_OBJECT()) AS status
+  ), JSON_OBJECT()) AS is_active_status
 FROM `Ap_Profiles` AS ap

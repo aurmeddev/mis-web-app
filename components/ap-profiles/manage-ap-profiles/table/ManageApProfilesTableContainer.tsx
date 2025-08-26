b0e44b000307bac7c0cfdc1bb357f9ef23f3cee3
Reymart <reymartpinedaj@gmail.com>
2025-08-27 01:04:36 +0800

    remove unnecessary columns and handle data population after submission

--- components/ap-profiles/manage-ap-profiles/table/ManageApProfilesTableContainer.tsx
+++ components/ap-profiles/manage-ap-profiles/table/ManageApProfilesTableContainer.tsx
@@ -4,35 +4,33 @@ import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { ManageApProfilesTable } from "./ManageApProfilesTable";
-import { SearchInput } from "../search/SearchInput";
 import { ManageApProfilesDialog } from "../dialog/ManageApProfilesDialog";
 import { useRouter } from "next/navigation";
 import { Pagination } from "@/components/pagination/route-based/Pagination";
 import { SearchResult } from "@/components/search/SearchResult";
+import { SearchInput } from "@/components/search/SearchInput";
+import { SearchQuery } from "@/components/otp-generator/type";
+import { ManageApProfilesSearchResults } from "../search/ManageApProfilesSearchResults";
+import { ApProfilesService } from "@/lib/features/ap-profiles/ApProfilesService";
+import { ApiResponseProps } from "@/database/dbConnection";
 
 type ManageApProfilesTableContainerProps = {
   response: any;
 };
 
-export type ManageApProfilesRecordRaw = ManageApProfilesForm & {
-  id: number;
-  team_id: number;
-  team_name: string;
-  created_at: string;
-  created_by: string;
-  is_active: number;
-};
-
-type ManageApProfilesForm = {
-  ip_address: string;
-  name: string;
-};
-
 type Pagination = { page: number; limit: number };
+
+type ProfileForm = {
+  profile_name: string;
+  fb_account_id?: number;
+  remarks?: "";
+};
 
 export function ManageApProfilesTableContainer({
   response,
 }: ManageApProfilesTableContainerProps) {
+  const profilesService = new ApProfilesService();
+
   const router = useRouter();
   const showToast = (isSuccess: boolean, message: string) => {
     if (isSuccess) {
@@ -44,21 +42,22 @@ export function ManageApProfilesTableContainer({
 
   const [editingRow, setEditingRow] = useState<number | null>(null);
   const [isAddingNew, setIsAddingNew] = useState(false);
-  const [form, setForm] = useState<any>({
+  const [form, setForm] = useState<ProfileForm>({
     profile_name: "",
-    fb_owner_name: "",
-    username: "",
-    password: "",
-    long_2fa_key: "",
-    recovery_codes: "",
+    fb_account_id: undefined,
     remarks: "",
-    is_active: 2,
   });
   const [tableData, setTableData] = useState<any>(response.data);
   const [isSubmitInProgress, setIsSubmitInProgress] = useState(false);
   const [hasStatusChanged, setHasStatusChanged] = useState(false);
-  const [isSearching, setIsSearching] = useState(false);
   const [open, setOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState<SearchQuery>({
+    query: "",
+    isSearching: false,
+    result: { data: [], isSuccess: false, message: "" },
+    selectedResult: null,
+  });
+  const [showResults, setShowResults] = useState(false);
 
   useEffect(() => {
     setTableData(response.data);
@@ -73,7 +72,7 @@ export function ManageApProfilesTableContainer({
 
   const handleStatusChange = (value: string) => {
     const selectedRecord = tableData.find(
-      (record: ManageApProfilesRecordRaw) => record.id === editingRow
+      (record: any) => record.id === editingRow
     );
     const originalValue = String(selectedRecord?.is_active);
     setForm((prevState: any) => ({
@@ -84,149 +83,97 @@ export function ManageApProfilesTableContainer({
     setHasStatusChanged(value !== originalValue);
   };
 
-  const handleConfirm = async () => {
-    const canRequest = hasInputChanged();
+  const handleSubmit = async (ev: any) => {
+    ev.preventDefault();
+
     setIsSubmitInProgress(true);
-
-    const { created_at, created_by, team_name, is_active, row_id, ...rest } =
-      form;
-
-    const updatePayload = rest as any;
-
-    // if (!isAddingNew && hasStatusChanged) {
-    //   const payload: any = {
-    //     ...rest,
-    //     is_active: Number(is_active),
-    //   };
-
-    //   const { isSuccess, message } = await ipWhitelistService.setActive(
-    //     payload
-    //   );
-
-    //   if (!isSuccess) {
-    //     showToast(false, message);
-    //     return;
-    //   }
-    // }
-
-    if (!canRequest) {
-      resetFormState();
-      return;
-    }
-
-    // const response = isAddingNew
-    //   ? await ipWhitelistService.post(form)
-    //   : await ipWhitelistService.update(updatePayload);
-    const response = { isSuccess: false, message: "", data: [] };
-
+    const payload: ProfileForm = {
+      profile_name: form.profile_name,
+      fb_account_id: form.fb_account_id,
+      remarks: form.remarks,
+    };
+    const response = await profilesService.post(payload);
     setIsSubmitInProgress(false);
-
     if (!response.isSuccess) {
-      showToast(false, response.message);
-      return;
+      toast.error(response.message);
+      return setOpen(false);
     }
 
-    if (isAddingNew) {
-      handleNewEntry(response);
-    } else {
-      handleUpdateEntry();
-    }
-
-    showToast(true, response.message);
+    handleNewEntry(response);
+    toast.success(response.message);
+    setOpen(false);
   };
 
-  const resetFormState = () => {
-    setIsAddingNew(false);
-    setEditingRow(null);
-    // setForm(null);
-    setIsSubmitInProgress(false);
-  };
-
-  const handleNewEntry = (response: any) => {
-    const data: { insertId: number } = response.data as any;
+  const handleNewEntry = (response: ApiResponseProps) => {
+    const { data } = response;
     const updatedForm = {
-      id: data.insertId,
-      ...form,
-      is_active: 1,
+      row_id: 1,
+      profile_name: form.profile_name,
+      fb_account: {
+        fb_owner_name: "",
+      },
+      created_by: {
+        full_name: data[0].created_by.full_name,
+        team_name: data[0].created_by.team_name,
+      },
+      created_at: data[0].created_at,
+      remarks: form.remarks,
+      status: data[0].status,
     };
 
-    setTableData((prevData: ManageApProfilesRecordRaw[]) => [
+    setTableData((prevData: any[]) => [
       updatedForm,
-      ...prevData,
+      ...prevData.map((row) => ({
+        ...row,
+        row_id: row.row_id + 1, // increment each old row_id by 1
+      })),
     ]);
-
-    setIsAddingNew(false);
-    setForm((prevState: any) => ({
-      ...prevState,
-      is_active: 1,
-    }));
   };
 
-  const handleUpdateEntry = () => {
-    setTableData((prevData: ManageApProfilesRecordRaw[]) =>
-      prevData.map((item) =>
-        item.id === form.id
-          ? { ...item, ...form, is_active: Number(form.is_active) }
-          : item
-      )
-    );
+  // const handleUpdateEntry = () => {
+  //   setTableData((prevData: ManageApProfilesRecordRaw[]) =>
+  //     prevData.map((item) =>
+  //       item.id === form.id
+  //         ? { ...item, ...form, is_active: Number(form.is_active) }
+  //         : item
+  //     )
+  //   );
 
-    setEditingRow(null);
-    setForm(null);
-  };
+  //   setEditingRow(null);
+  // };
 
   const handleEditChange = (id: number | null) => {
-    if (id === null) {
-      setIsAddingNew(false);
-    }
-    const selectedWhitelistData = response.data.find(
-      (data: { id: number }) => data.id === id
-    );
-    setEditingRow(id);
-    if (selectedWhitelistData) {
-      setForm(selectedWhitelistData);
-    }
+    // if (id === null) {
+    //   setIsAddingNew(false);
+    // }
+    // const selectedWhitelistData = response.data.find(
+    //   (data: { id: number }) => data.id === id
+    // );
+    // setEditingRow(id);
+    // if (selectedWhitelistData) {
+    //   setForm(selectedWhitelistData);
+    // }
   };
 
-  const hasInputChanged = () => {
-    if (isAddingNew) return true;
-    const selectedRecord = tableData.find(
-      (record: ManageApProfilesRecordRaw) => record.id === editingRow
-    );
+  // const hasInputChanged = () => {
+  //   if (isAddingNew) return true;
+  //   const selectedRecord = tableData.find(
+  //     (record: ManageApProfilesRecordRaw) => record.id === editingRow
+  //   );
 
-    if (selectedRecord) {
-      const ip_address = selectedRecord["ip_address"];
-      const name = selectedRecord["name"];
-      const is_active = selectedRecord["is_active"];
+  //   if (selectedRecord) {
+  //     const ip_address = selectedRecord["ip_address"];
+  //     const name = selectedRecord["name"];
+  //     const is_active = selectedRecord["is_active"];
 
-      const hasChanged =
-        ip_address !== form?.ip_address ||
-        name !== form?.name ||
-        is_active !== form?.is_active;
-      return hasChanged;
-    }
-    return false;
-  };
-
-  const handleSearch = async (value: string) => {
-    setIsSearching(true);
-    if (!value) {
-      setTableData(response.data);
-      setIsSearching(false);
-      return;
-    }
-
-    try {
-      //   const { data } = await ipWhitelistService.find({ searchKey: value });
-      const { data } = { data: [] };
-
-      if (data?.length) {
-        setTableData(data);
-      }
-    } finally {
-      setIsSearching(false); // ✅ Always reset after request completes
-    }
-  };
+  //     const hasChanged =
+  //       ip_address !== form?.profile_name ||
+  //       name !== form?.fb_account_id ||
+  //       is_active !== form?.remarks;
+  //     return hasChanged;
+  //   }
+  //   return false;
+  // };
 
   const handlePagination = (page: number, limit: number) => {
     router.push(`?page=${page}&limit=${limit}`);
@@ -236,15 +183,19 @@ export function ManageApProfilesTableContainer({
   const total_pages = response.pagination?.total_pages;
   const limit = response.pagination?.limit || 10;
 
+  const handleNewProfile = () => {
+    setForm({ profile_name: "", fb_account_id: undefined, remarks: "" });
+    setOpen(true);
+  };
+
   return (
     <>
       <ManageApProfilesDialog
         form={form}
-        rowData={[]}
         open={open}
         setOpen={setOpen}
         editingRow={editingRow}
-        handleConfirm={handleConfirm}
+        handleSubmit={handleSubmit}
         handleEditChange={handleEditChange}
         handleInputChange={handleInputChange}
         handleStatusChange={handleStatusChange}
@@ -254,18 +205,26 @@ export function ManageApProfilesTableContainer({
         <Button
           className="cursor-pointer h-8 text-white"
           variant="default"
-          onClick={() => setOpen(true)}
+          onClick={handleNewProfile}
         >
           New Profile Entry
         </Button>
-        <SearchInput
-          placeholder="Search by profile"
-          onSearch={handleSearch}
-          isSearching={isSearching}
-        />
-        <SearchResult setShowResults={() => {}}>
-          <div></div>
-        </SearchResult>
+
+        <div className="relative w-full">
+          <SearchInput
+            searchQuery={searchQuery}
+            onSearchQueryChange={() => {}}
+            onSearchFocus={() => {}}
+          />
+          {showResults && (
+            <SearchResult setShowResults={setShowResults}>
+              <ManageApProfilesSearchResults
+                result={searchQuery.result}
+                handleSelectItem={() => {}}
+              />
+            </SearchResult>
+          )}
+        </div>
       </div>
       <ScrollArea className="h-[75dvh] mt-4">
         <ManageApProfilesTable
@@ -273,7 +232,7 @@ export function ManageApProfilesTableContainer({
           data={tableData}
           addMode={isAddingNew}
           editingRow={editingRow}
-          handleConfirm={handleConfirm}
+          handleConfirm={() => {}}
           handleEditChange={handleEditChange}
           handleInputChange={handleInputChange}
           handleStatusChange={handleStatusChange}

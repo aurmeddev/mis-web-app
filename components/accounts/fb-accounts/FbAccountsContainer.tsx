c0106c579323726c8eb84d4ac426787729cda1c2
Reymart <reymartpinedaj@gmail.com>
2025-09-03 12:13:37 +0800

    feat: refactor AP and FB accounts management components to use a unified TableFetch component

--- components/accounts/fb-accounts/FbAccountsContainer.tsx
+++ components/accounts/fb-accounts/FbAccountsContainer.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from "react";
 import { TableLoader } from "@/components/skeleton-loader/TableLoader";
-import { FbAccountsTableFetch } from "./table/FbAccountsTableFetch";
+import { TableFetch } from "@/components/table/server-fetch/TableFetch";
+import { FbAccountsService } from "@/lib/features/fb-accounts/FbAccountsService";
+import { FbAccountsTableContainer } from "./table/FbAccountsTableContainer";
 
 type Props = {
   searchParams: { page: number; limit: number };
 };
 
 export async function FbAccountsContainer({ searchParams }: Props) {
+  const fbAccountsService = new FbAccountsService();
+
   return (
     <div className="min-h-[calc(100dvh-7rem)] p-6 pr-0">
       <div>
@@ -14,7 +18,11 @@ export async function FbAccountsContainer({ searchParams }: Props) {
         <p className="text-sm">Securely Manage Your Fb Accounts</p>
       </div>
       <Suspense fallback={<TableLoader />}>
-        <FbAccountsTableFetch searchParams={searchParams} />
+        <TableFetch
+          searchParams={searchParams}
+          fetchService={(params) => fbAccountsService.getAll(params)}
+          Container={FbAccountsTableContainer}
+        />
       </Suspense>
     </div>
   );

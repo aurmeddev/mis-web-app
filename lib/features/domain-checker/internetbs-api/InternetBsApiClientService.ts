7a588b736d0905748a83e28b4e99c3953bb17d9d
Jay Lord <pagal.jaylord@gmail.com>
2025-09-06 00:03:58 +0800

    feat: integrated internetbs api in the MIS

---
+++ lib/features/domain-checker/internetbs-api/InternetBsApiClientService.ts
@@ -0,0 +1,22 @@
+import { ApiResponseProps } from "@/database/dbConnection";
+import { SearchParamsManager } from "@/lib/utils/search-params/SearchParamsManager";
+export class InternetBsApiClientService {
+  async getDomainInfo(params: { domain: string }): Promise<ApiResponseProps> {
+    const searchParams = new SearchParamsManager();
+    const searchQueryParams = searchParams.append(params);
+    const baseURL = `${process.env.NEXT_AURMED_BACKEND_API_BASE_URL}`;
+    const secretAuthKey = `Bearer ${process.env.NEXT_AURMED_BACKEND_SERVER_ACCESS_TOKEN}`;
+    const response = await fetch(
+      `${baseURL}/internetbs/domain-info${searchQueryParams}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: secretAuthKey,
+        },
+        cache: "no-store",
+      }
+    );
+    return await response.json();
+  }
+}

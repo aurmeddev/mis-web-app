f774b1639d09a654c4ad68c2e3f2bba47a6c05eb
Reymart <reymartpinedaj@gmail.com>
2025-09-17 17:24:11 +0800

    add shared component for local file downloading

---
+++ components/shared/download/DownloadLocalFile.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+
+type Props = {
+  className?: string;
+  text: string;
+  fileNameWithExt: string; //eg: file.csv
+  url: string;
+};
+
+export default function DownloadLocalFile({
+  className,
+  fileNameWithExt,
+  text,
+  url,
+}: Props) {
+  return (
+    <a
+      href={url}
+      download={fileNameWithExt}
+      className={cn("hover:underline text-blue-500 text-sm", className)}
+    >
+      {text}
+    </a>
+  );
+}

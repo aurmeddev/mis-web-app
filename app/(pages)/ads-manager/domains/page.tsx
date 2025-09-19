aeef900e64e14b1c865cbd779f8b21ac1207d211
Reymart <reymartpinedaj@gmail.com>
2025-09-19 09:48:31 +0800

    fix: domains page cannot be access by other user with granted access

--- app/(pages)/ads-manager/domains/page.tsx
+++ app/(pages)/ads-manager/domains/page.tsx
@@ -1,7 +1,5 @@
 import { DomainsContainer } from "@/components/ads-manager/domains/DomainsContainer";
 import { NotFound } from "@/components/not-found/not-found";
-import { CryptoUtilsManager } from "@/lib/features/security/cryptography/util/CryptoUtilsManager";
-import { CryptoUtilsServerService } from "@/lib/features/security/cryptography/util/CryptoUtilsServerService";
 import { getSession } from "@/lib/features/security/user-auth/jwt/JwtAuthService";
 
 export default async function Page({ searchParams }: any) {
@@ -14,12 +12,6 @@ export default async function Page({ searchParams }: any) {
 
   if (!session) return <NotFound param={notFoundObj} />;
 
-  const encryptedTeamId = session.user.id;
-  const decipher = new CryptoUtilsManager(new CryptoUtilsServerService());
-  const decipheredTeamId = (
-    await decipher.cryptoArrayString({ data: encryptedTeamId })
-  ).string();
-
   const awaitedParams = await searchParams;
   const page = Number(awaitedParams.page) || 1;
   const limit = Number(awaitedParams.limit) || 50;
@@ -28,12 +20,5 @@ export default async function Page({ searchParams }: any) {
     limit,
   };
 
-  /*
-   *User ID 1 is the Super Admin Account
-   */
-  const IS_USER_SUPER_ADMIN = decipheredTeamId === "1";
-  if (!IS_USER_SUPER_ADMIN) {
-    return <NotFound param={notFoundObj} />;
-  }
   return <DomainsContainer searchParams={params} />;
 }

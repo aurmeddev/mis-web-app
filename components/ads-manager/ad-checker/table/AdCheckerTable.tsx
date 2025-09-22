401ea75cacffd6be6840ac93c9d1c2fd29dc7a40
Reymart <reymartpinedaj@gmail.com>
2025-09-22 13:25:12 +0800

    update ad checker table to text-xs

--- components/ads-manager/ad-checker/table/AdCheckerTable.tsx
+++ components/ads-manager/ad-checker/table/AdCheckerTable.tsx
@@ -19,6 +19,7 @@ import { AdData } from "../AdCheckerContainer";
 import { useAdColumns } from "../column/useAdColumns";
 import { useState } from "react";
 import { Pagination } from "@/components/shared/pagination/tanstack/Pagination";
+import { TableWithResizableHeader } from "@/components/shared/table/with-resizable-header/TableWithResizableHeader";
 
 type Props = {
   tableData: AdData[];
@@ -57,51 +58,7 @@ export function AdCheckerTable({ onViewCreatives, tableData }: Props) {
   return (
     <div className="flex flex-col overflow-auto w-full">
       <div className="border overflow-auto rounded w-full">
-        <Table style={{ width: table.getTotalSize() }}>
-          <TableHeader className="bg-muted">
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <TableHead
-                    key={header.id}
-                    style={{ width: header.getSize() }}
-                    className="border-r relative"
-                  >
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                    {header.column.getCanResize() && (
-                      <div
-                        onMouseDown={header.getResizeHandler()}
-                        onTouchStart={header.getResizeHandler()}
-                        className="absolute right-0 top-0 h-full w-px cursor-col-resize select-none hover:bg-primary hover:w-1"
-                      />
-                    )}
-                  </TableHead>
-                ))}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell
-                    key={cell.id}
-                    className="border-r"
-                    style={{
-                      width: cell.column.getSize(),
-                      minWidth: cell.column.columnDef.minSize,
-                    }}
-                  >
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        <TableWithResizableHeader className="text-xs" table={table} />
       </div>
       {tableData.length ? (
         <Pagination table={table} tableDataLength={tableData.length} />

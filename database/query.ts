93aefcb2704531628bd92592f4f12d5b4eb6bafc
Jay Lord <pagal.jaylord@gmail.com>
2025-10-02 23:13:10 +0800

    feat: created pingDatabaseConnection and keep-tcp-alive cron job

--- database/query.ts
+++ database/query.ts
@@ -1,36 +1,3 @@
-// import { pool } from "./pool";
-
-// export async function query({
-//   query,
-//   values,
-// }: {
-//   query: string;
-//   values: string[];
-// }) {
-//   let connection;
-//   try {
-//     connection = await pool.getConnection();
-//     const [rows] = await connection.execute(query, values);
-//     return rows;
-//   } catch (error) {
-//     console.error("Database query failed:", error);
-//     throw error;
-//   } finally {
-//     // Release the Connection Back to the Pool
-//     // THIS IS CRUCIAL! The connection is NOT closed, but returned to the pool for reuse.
-//     if (connection) {
-//       console.log("Releasing connection back to the pool");
-//       connection.release();
-//     }
-//   }
-// }
-
-// export type ApiResponseProps = {
-//   isSuccess: boolean;
-//   data: any[];
-//   message: string;
-// };
-
 import { isEnvProduction } from "@/lib/env/isEnvProduction";
 import mysql, { ConnectionOptions } from "mysql2/promise";
 
@@ -38,8 +5,8 @@ export async function query({
   query,
   values,
 }: {
-  query: any;
-  values: string[];
+  query: string;
+  values: any[];
 }) {
   const access: ConnectionOptions = {
     host: process.env.NEXT_DBHOST,

9750728ddff5e111aa18bd1376d1f2d3730acf52
Jay Lord <pagal.jaylord@gmail.com>
2025-11-21 13:37:03 +0800

    update: optimized v_Recruiters.sql

--- database/views/v_Recruiters.sql
+++ database/views/v_Recruiters.sql
@@ -1,13 +1,8 @@
 WITH DistinctRecruiters AS (
-    SELECT DISTINCT(recruited_by) AS recruited_by FROM `Fb_Accounts` 
-),
-Totals AS (
-    SELECT dr.recruited_by, COUNT(id) as totals 
-	FROM `v_FbAccountsV2` AS v_fb
-	INNER JOIN DistinctRecruiters AS dr 
-    ON dr.recruited_by=v_fb.recruiter
-    GROUP BY dr.recruited_by
+    SELECT DISTINCT(recruited_by) AS recruited_by, COUNT(id) as totals 
+	FROM `Fb_Accounts` AS fb
+    GROUP BY fb.recruited_by
 )
-SELECT u.id, u.full_name, u.display_name, total.totals FROM `Users` AS u 
-INNER JOIN Totals AS total
-ON total.recruited_by=u.id;
+SELECT u.id, u.full_name, u.display_name, dr.totals FROM `Users` AS u 
+INNER JOIN DistinctRecruiters AS dr
+ON dr.recruited_by=u.id;
